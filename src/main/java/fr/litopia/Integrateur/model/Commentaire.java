c3bdc1cc7f2e047da884369e0b97220e99114b33
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-04-30 09:52:54 +0200

    Feat : Encapsulation de la classe commentaire

--- src/main/java/fr/litopia/Integrateur/model/Commentaire.java
+++ src/main/java/fr/litopia/Integrateur/model/Commentaire.java
@@ -1,8 +1,5 @@
 package fr.litopia.Integrateur.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,18 +7,27 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Commentaire {
-    @Getter
-    @Setter
     @Id
     @Column(name = "idCom", nullable = false)
     Integer id;
 
-    @Column(name = "text", nullable = false)
+    @Column(name = "comentaire", nullable = false, length = 128)
     String text;
 
-    @ManyToOne
-    @Column(name = "Auteur-du-commentaire ", nullable = false)
+    @ManyToOne(optional = false)
     Chami auteur;
 
-
+    public Commentaire() {}
+    Commentaire(String text, Chami auteur) {
+        this.auteur = auteur;
+    }
+    public void setText(String text) {
+        if(text.length() > 128){
+            throw new IllegalArgumentException("Commentaire should be less than 128 characters");
+        }
+        this.text = text;
+    }
+    public String getText() {
+        return text;
+    }
 }
