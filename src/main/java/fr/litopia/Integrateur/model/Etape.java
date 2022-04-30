6051ee537c87016fc00b6fa321a91cc9c834deb8
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-04-30 10:58:49 +0200

    Feat : Encapsulation de Etape

--- src/main/java/fr/litopia/Integrateur/model/Etape.java
+++ src/main/java/fr/litopia/Integrateur/model/Etape.java
@@ -1,24 +1,47 @@
 package fr.litopia.Integrateur.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
 
 @Entity
 @Inheritance(strategy= InheritanceType.JOINED)
-public class Etape {
+public abstract class Etape {
     @Id
     @Column(name = "id", nullable = false)
     public Long id;
 
-    @Column(name = "titre", nullable = false)
-    public String titre;
-
-    @Column(name = "description", nullable = false)
-    public String description;
-
     @Column(name = "numero", nullable = false)
+    @Min(0)
     public Integer numero;
 
-    @Column(name = "statut", nullable = false)
-    public StatutEtape statut;
+    @Column(name = "titre", nullable = false, length = 32)
+    public String titre;
 
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        if (numero == null) {
+            throw new IllegalArgumentException("Numero cannot be null");
+        }
+        if (numero < 0) {
+            throw new IllegalArgumentException("Numero cannot be negative");
+        }
+        this.numero = numero;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        if (titre == null || titre.isEmpty()) {
+            throw new IllegalArgumentException("Titre cannot be null or empty");
+        }
+        if (titre.length() > 32) {
+            throw new IllegalArgumentException("Titre cannot be longer than 32 characters");
+        }
+        this.titre = titre;
+    }
 }
---
+++ src/test/java/fr/litopia/Integrateur/model/EtapeTest.java
@@ -0,0 +1,38 @@
+package fr.litopia.Integrateur.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EtapeTest {
+
+    @Test
+    void setNumero() {
+        //Use tache class cause we need to instantiate a subclass of Etape to test his method
+        Tache tache = new Tache();
+        tache.setNumero(0);
+        assertEquals(0, tache.getNumero());
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> tache.setNumero(null));
+        assertEquals("Numero cannot be null", e.getMessage());
+
+        e = assertThrows(IllegalArgumentException.class, () -> tache.setNumero(-1));
+        assertEquals("Numero cannot be negative", e.getMessage());
+    }
+
+    @Test
+    void setTitre() {
+        Tache tache = new Tache();
+        tache.setTitre("titre");
+        assertEquals("titre", tache.getTitre());
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> tache.setTitre(null));
+        assertEquals("Titre cannot be null or empty", e.getMessage());
+
+        e = assertThrows(IllegalArgumentException.class, () -> tache.setTitre(""));
+        assertEquals("Titre cannot be null or empty", e.getMessage());
+
+        e = assertThrows(IllegalArgumentException.class, () -> tache.setTitre("a".repeat(33)));
+        assertEquals("Titre cannot be longer than 32 characters", e.getMessage());
+    }
+}
