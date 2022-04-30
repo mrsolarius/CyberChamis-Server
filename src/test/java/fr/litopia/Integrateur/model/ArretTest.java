9a840f063d82ae6daf6ed6e7d2e6666cac3177e9
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-04-30 09:49:46 +0200

    Refactor : Rename des tests

--- src/test/java/fr/litopia/Integrateur/model/ArretTest.java
+++ src/test/java/fr/litopia/Integrateur/model/ArretTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class TestArret {
+public class ArretTest {
 
     private static Arret a;
 
