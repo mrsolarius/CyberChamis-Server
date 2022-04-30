85c3ebfa2a0db6277234e27d9f6e266c21e41d2b
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-04-30 14:59:08 +0200

    Feat : Encapsulation de la classe Visite @TODO faire les test

--- src/main/java/fr/litopia/Integrateur/model/Tache.java
+++ src/main/java/fr/litopia/Integrateur/model/Tache.java
@@ -4,8 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.Min;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 public class Tache extends Etape {
@@ -77,6 +76,12 @@ public class Tache extends Etape {
 
     public Set<Indice> getIndices() {
         return indices;
+    }
+
+    public List<Indice> getSortedIndices() {
+        List<Indice> sortEtapes = new ArrayList<>(indices);
+        sortEtapes.sort(Comparator.comparing(Indice::getNumIndice));
+        return sortEtapes;
     }
 
     public void addIndice(Indice indice) {
--- src/main/java/fr/litopia/Integrateur/model/Visite.java
+++ src/main/java/fr/litopia/Integrateur/model/Visite.java
@@ -5,11 +5,10 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Visite {
-    @Getter
-    @Setter
     @Id
     @Column(name = "id", nullable = false)
     public Long id;
@@ -23,46 +22,96 @@ public class Visite {
     @Column(name = "etapeCourante", nullable = false)
     public int etapeCourante;
 
+    @OneToOne
+    public Defi defi;
+
     @OneToMany
-    @Column(name = "etapes", nullable = false)
-    public List<Etape> etapes;
+    public Set<Reponse> reponses;
 
-    /* retourne l'etape courante à partir de son id */
-    public Etape getEtapeCourante() throws IndexOutOfBoundsException {
-        return etapes.get(etapeCourante);
+    private Reponse getReponse(int numero) {
+        if(numero < 0 || numero > reponses.size()){
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        for (Reponse reponse : reponses) {
+            if (reponse.numero == numero) {
+                return reponse;
+            }
+        }
+        return null;
     }
 
-    /*
-    public void etapeSuivante() {
-        getEtapeCourante().statut = StatutEtape.FINISHED;
-        etapeCourante += 1;
-        getEtapeCourante().statut = StatutEtape.CURRENT;
+    /**
+     *  retourne l'etape courante à partir de son id
+     * @return Etape l'etape courante
+     */
+    public Etape getEtapeCourante(){
+        return defi.getSortEtapes().get(etapeCourante);
     }
 
-    public void etapePrecedente() {
-        getEtapeCourante().statut = StatutEtape.WAITING;
-        etapeCourante -= 1;
-        getEtapeCourante().statut = StatutEtape.CURRENT;
+    /**
+     * actualiser etapeCourante
+     * si c'est une tache : création d'une réponse
+     * throw une erreur si il n'y a pas d'étape suivante
+     */
+    public Etape etapeSuivante() {
+        if(statut != StatutVisite.ENCOURS){
+            throw new RuntimeException("Visite not in progress");
+        }
+        if(etapeCourante +1 > defi.getSortEtapes().size() - 1){
+            throw new RuntimeException("No more steps");
+        }
+        etapeCourante++;
+        Etape currentEtape = getEtapeCourante();
+        if(currentEtape instanceof Tache){
+            Reponse reponse = new Reponse(etapeCourante);
+            this.reponses.add(reponse);
+        }
+        return currentEtape;
+    }
+
+    public Etape etapePrecedente() {
+        if(statut != StatutVisite.ENCOURS){
+            throw new RuntimeException("Visite not in progress");
+        }
+        if(etapeCourante -1 < 0){
+            throw new RuntimeException("No previous steps");
+        }
+        etapeCourante--;
+        return getEtapeCourante();
     }
 
     public void setStatut(StatutVisite statut) throws Exception {
-        if (statut == StatutVisite.FINISHED) {
-            for (Etape etape : etapes) {
-                if (etape.statut != StatutEtape.FINISHED) {
-                    throw new Exception("Statut Visite Fini impossible : toutes les étapes ne sont pas terminées.");
+        if(this.statut == StatutVisite.FINISHED || this.statut == StatutVisite.ABONDON){
+            throw new IllegalArgumentException("Visite already finished");
+        }
+        switch (statut) {
+            case FINISHED -> {
+                if (etapeCourante != defi.getSortEtapes().size() - 1) {
+                    throw new RuntimeException("Could not finish the visite while not at the end of the defi");
                 }
+                calculPoints();
             }
-            calculPoints();
-        } else if (statut == StatutVisite.ABONDON) {
-            calculPoints();
+            case ABONDON, PAUSE -> calculPoints();
         }
         this.statut = statut;
     }
 
     public void calculPoints() {
-        for (Etape etape :
-             etapes) {
-            //trouver un moyen de compter les points des taches
+        Integer tempPoints = 0;
+        for (Etape etape : defi.getSortEtapes()) {
+            if (etape instanceof Tache) {
+                Tache tache = (Tache) etape;
+                Reponse reponse = getReponse(etape.getNumero());
+                if(tache.isSecret(this.getReponse(etape.getNumero()).getReponseUtilisateur())){
+                    tempPoints += tache.getPoint();
+                    if(reponse.getNbIndicesUtilises()>0){
+                        for(int i = 0; i < reponse.getNbIndicesUtilises(); i++){
+                            tempPoints -= tache.getSortedIndices().get(i).getPointsPerdus();
+                        }
+                    }
+                };
+            }
         }
-    }*/
+        this.points = tempPoints;
+    }
 }
