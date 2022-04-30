ba2435b1a3b79abd1da6f4562930c0846a3825dc
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-04-30 14:10:29 +0200

    Feat : Encapsulation de la classe Reponse

---
+++ src/main/java/fr/litopia/Integrateur/model/Reponse.java
@@ -0,0 +1,62 @@
+package fr.litopia.Integrateur.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+
+@Entity
+public class Reponse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    public Long id;
+
+    @Min(0)
+    @Column(name = "numero", nullable = false)
+    public Integer numero;
+
+    @Min(0)
+    @Column(name = "score", nullable = false)
+    public Integer nbIndicesUtilises;
+
+    @Column(name = "texte", nullable = false)
+    public String reponseUtilisateur;
+
+    public Reponse(){
+        this.nbIndicesUtilises = 0;
+    }
+
+    public Reponse(Integer numero){
+        this();
+        if (numero < 0) {
+            throw new IllegalArgumentException("Numero must be positive");
+        }
+        this.numero = numero;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getNbIndicesUtilises() {
+        return nbIndicesUtilises;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public String getReponseUtilisateur() {
+        return reponseUtilisateur;
+    }
+
+    public void addIndiceUtilise() {
+        nbIndicesUtilises++;
+    }
+
+    public void setReponseUtilisateur(String reponseUtilisateur) {
+        if(this.reponseUtilisateur != null) {
+            throw new IllegalArgumentException("Reponse could not be changed");
+        }
+        this.reponseUtilisateur = reponseUtilisateur;
+    }
+}
---
+++ src/test/java/fr/litopia/Integrateur/model/ReponseTest.java
@@ -0,0 +1,36 @@
+package fr.litopia.Integrateur.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReponseTest {
+
+    private Reponse reponse;
+
+    @BeforeEach
+    void setUp() {
+        reponse = new Reponse();
+    }
+
+    @Test
+    void addIndiceUtilise() {
+        assertEquals(0, reponse.getNbIndicesUtilises());
+
+        reponse.addIndiceUtilise();
+        assertEquals(1, reponse.getNbIndicesUtilises());
+
+        reponse.addIndiceUtilise();
+        assertEquals(2, reponse.getNbIndicesUtilises());
+    }
+
+    @Test
+    void setReponseUtilisateur() {
+        reponse.setReponseUtilisateur("Bonjour");
+        assertEquals("Bonjour", reponse.getReponseUtilisateur());
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> reponse.setReponseUtilisateur("Au revoir"));
+        assertEquals("Reponse could not be changed", e.getMessage());
+    }
+}
