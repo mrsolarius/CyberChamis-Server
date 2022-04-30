ddb5120c167e0afd49dbdaa0869b8ab8c6b056fa
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-04-30 09:26:15 +0200

    Feat : Encapsulation de la classe Arret

--- src/main/java/fr/litopia/Integrateur/model/Arret.java
+++ src/main/java/fr/litopia/Integrateur/model/Arret.java
@@ -1,27 +1,59 @@
 package fr.litopia.Integrateur.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 
 @Entity
 public class Arret {
-    @Getter
-    @Setter
     @Id
-    //@OneToOne
-    @Column(name = "codeArret", nullable = false)
+    @Column(name = "codeArret", nullable = false, length = 12)
     public String codeArret;
 
-    @Column(name = "nomArret", nullable = false)
+    @Column(name = "nomArret", nullable = false, length = 50)
     public String nomArret;
 
     @Column(name = "streetMapUrl", nullable = false)
     public String streetMap;
 
+    Arret(String codeArret, String nomArret, String streetMap) {
+        this.setCodeArret(codeArret);
+        this.setNomArret(nomArret);
+        this.setStreetMap(streetMap);
+    }
 
+    public Arret() {
+    }
+
+    public void setCodeArret(String codeArret) {
+        if(codeArret.length() > 12) {
+            throw new IllegalArgumentException("CodeArret must be 12 characters long");
+        }
+        this.codeArret = codeArret;
+    }
+    public void setNomArret(String nomArret) {
+        if(nomArret.length() > 50) {
+            throw new IllegalArgumentException("NomArret must be 50 characters long");
+        }
+        this.nomArret = nomArret;
+    }
+
+    public void setStreetMap(String streetMap) {
+        if (streetMap.length() > 256) {
+            throw new IllegalArgumentException("StreetMap must be 256 characters long");
+        }
+        this.streetMap = streetMap;
+    }
+
+    public String getCodeArret() {
+        return codeArret;
+    }
+
+    public String getNomArret() {
+        return nomArret;
+    }
+
+    public String getStreetMap() {
+        return streetMap;
+    }
 }
