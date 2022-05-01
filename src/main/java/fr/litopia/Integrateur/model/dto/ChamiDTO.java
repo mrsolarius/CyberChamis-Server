5ff47ce08bc0885b7f1f8a04a3bb625b5bd95cc7
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-05-01 18:03:41 +0200

    Refactor : Supression du code mort

--- src/main/java/fr/litopia/Integrateur/model/dto/ChamiDTO.java
+++ src/main/java/fr/litopia/Integrateur/model/dto/ChamiDTO.java
@@ -1,26 +1,8 @@
 package fr.litopia.Integrateur.model.dto;
 
-import fr.litopia.Integrateur.model.dto.interfaces.DTO;
-import fr.litopia.Integrateur.model.entity.Chami;
-import fr.litopia.Integrateur.model.entity.Defi;
-
-public class ChamiDTO implements DTO<Chami> {
+public class ChamiDTO {
     public long id;
     public Integer age;
     public String bio;
     public String username;
-
-    @Override
-    public Chami toEntity() {
-        Chami c = new Chami();
-        if (id != 0)
-            c.id = this.id;
-        if (age != null)
-            c.age=this.age;
-        if (bio != null)
-            c.bio=this.bio;
-        if (username != null)
-            c.username=this.username;
-        return c;
-    }
 }
--- src/main/java/fr/litopia/Integrateur/model/dto/interfaces/DTO.java
+++
@@ -1,8 +0,0 @@
-package fr.litopia.Integrateur.model.dto.interfaces;
-
-import fr.litopia.Integrateur.model.entity.Defi;
-
-public interface DTO<T>{
-
-    T toEntity();
-}
