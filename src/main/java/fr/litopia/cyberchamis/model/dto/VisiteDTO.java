d5a7bd8ea7e2d3382bdde27cf11c36857f37f8ba
BARKAOUI <meriam.barkaoui@etu.univ-grenoble-alpes.fr>
2022-05-05 11:16:57 +0200

    Petite modif

--- src/main/java/fr/litopia/cyberchamis/model/dto/VisiteDTO.java
+++ src/main/java/fr/litopia/cyberchamis/model/dto/VisiteDTO.java
@@ -6,9 +6,27 @@ import java.util.List;
 
 public class VisiteDTO {
     public Long id;
-    public StatutVisite statut;
+    public String statut;
     public EtapeDTO etapeCourante;
     public Integer points;
     public DefiDTO defi;
-    public List<ReponseDTO> reponse;
-}
+    public List<ReponseDTO> reponses;
+
+
+    /*public Visite toEntity(){
+        Visite visite =  new Visite();
+        visite.id = this.id;
+        visite.points=this.points;
+        visite.statut= StatutVisite.valueOf(this.statut);
+        //louis doit le faire
+        /*visite.etapeCourante=this;
+        visite.defi=this.defi.;
+        visite.reponses = this.reponses.stream().map(ReponseDTO::toEntity).collect(Collectors.toList());
+        List<String> l1 = new ArrayList<String>();
+        for (Reponse r : visite.reponses) {
+            l1.add(r.getId());
+        }
+        visite.reponses.add(l1);
+        return visite; */
+    }
+
