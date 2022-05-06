f9f23972eea29abb3a5ac41c3b956acedeaebb57
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-05-06 16:31:45 +0200

    Feat : ajout de la configuration du cross origin

--- src/main/java/fr/litopia/cyberchamis/RestServer.java
+++ src/main/java/fr/litopia/cyberchamis/RestServer.java
@@ -3,7 +3,13 @@ package fr.litopia.cyberchamis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"fr.litopia.cyberchamis.model"})
@@ -12,5 +18,18 @@ public class RestServer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServer.class, args);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("https://grecha-b8fd9.web.app/"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+				"Cache-Control", "Content-Type"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
